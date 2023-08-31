a61a55941211bb0fc2c3aa3bcd27ca4d1603e37d
ShornaEl <sabrinahossain761@gmail.com>
2023-08-31 06:59:09 +0600

    Final Commit

---
+++ app/src/main/java/com/example/loginpage/SignUp.java
@@ -0,0 +1,27 @@
+package com.example.loginpage;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class SignUp extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sign_up);
+        Button btn = (Button) findViewById(R.id.back);
+        Intent signup_login;
+        signup_login = new Intent(SignUp.this,MainActivity.class);
+
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(signup_login);
+            }
+        });
+    }
+}
